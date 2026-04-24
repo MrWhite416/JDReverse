8e719ef84d93fd93a7e63a9c4e321bc6ef3eb0c6
元英 <2673137332@qq.com>
2026-04-24 19:00:50 +0800

    第九部分 算法分析

--- 00.py
+++ 00.py
@@ -1,38 +1,10 @@
-import requests
-import base64
-import cv2
-import numpy as np
 
-headers = {
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-    "authorization": "Bearer 2OW94XiXDyH72LOVhvkxyUg5tlTrRCh9PdQTfYYDuva4zYX7ONPHN9",
-    "cache-control": "no-cache",
-    "origin": "https://www.rdv-prefecture.interieur.gouv.fr",
-    "pragma": "no-cache",
-    "priority": "u=1, i",
-    "referer": "https://www.rdv-prefecture.interieur.gouv.fr/",
-    "sec-ch-ua": "\"Microsoft Edge\";v=\"147\", \"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"147\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36 Edg/147.0.0.0"
-}
-url = "https://api.piste.gouv.fr/piste/captchetat/v2/simple-captcha-endpoint"
-params = {
-    "get": "image",
-    "c": "captchaFR"
-}
-response = requests.get(url, headers=headers, params=params)
 
-imageb64 = response.json()["imageb64"].replace("data:image/png;base64,", "")
-img = base64.b64decode(imageb64)
 
-nparr = np.frombuffer(img, np.uint8)
-frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+import hashlib
 
-cv2.imshow("1212", frame)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+s = "12"
+
+sha = hashlib.sha256(s.encode('utf-8'))
+print(sha.hexdigest())
--- 000.js
+++ 000.js
@@ -1,6 +1,529 @@
 
+var _$Q = [
+    "r2vUzxjHDg9YrNvUy3rPB24",
+    "zw50CMLLCW",
+    "ChaX",
+    "qMfKifbYB21PC2uGy29UC3rYDwn0B3i",
+    "mhWZFdr8nxWYFde",
+    "x19JB3jLlwPZx3nOyxjLzf9F",
+    "DZiZ",
+    "C2LNBIbLBgfWC2vKihrPBwuH",
+    "B2jQzwn0",
+    "y29UC3rYDwn0B3i",
+    "qxjYyxKGsxrLCMf0B3i",
+    "CMvQzwn0Aw9UsgfUzgXLza",
+    "Dw5JDa",
+    "C2v0",
+    "mdm4ns0WnY0YnvqWnZOWnJOZos45otLA",
+    "D2L0Ag91DfnLDhrLCG",
+    "D3vYoG",
+    "igLZig5VDcbHBIbVyMPLy3q",
+    "C3bSAxq",
+    "DxjS",
+    "ue9tva",
+    "w14/xsO",
+    "xsSK",
+    "yxbWBgLJyxrPB24VEc13D3CTzM9YBs11CMXLBMnVzgvK",
+    "CgfYyw1ZigLZigvTChr5igfMDgvYigv4y2X1zgLUzYaIDw5ZywzLiIbWyxjHBxm",
+    "AwzYyw1L",
+    "u3LTyM9Ska",
+    "zg9JDw1LBNrfBgvTzw50",
+    "C2nYAxb0",
+    "DZiY",
+    "ChDKDf9Pza",
+    "Aw5JBhvKzxm",
+    "Dw5PzM9YBu9MzNnLDa",
+    "t2jQzwn0",
+    "CgfYyw1ZigLZig5VDcbHihbSywLUig9IAMvJDa",
+    "WQKGmJaXnc0Ymdi0ierLBMLZifb1C2HRyxjLDIaOEMXVAxjVy2SUCNuP",
+    "x19Yzxf1zxn0qwXNB3jPDgHTt25JzsbRzxK6",
+    "lgTLEt0",
+    "DgHLBG",
+    "mhGXnG",
+    "ns4Z",
+    "EwvZ",
+    "lcbJAgvJAYbZDg9YywDLigzWoG",
+    "tw96AwXSys81lJaGxcGOlIO/kvWP",
+    "DgvZDcbLCNi",
+    "sw5JB3jYzwn0igLUDM9JyxrPB24",
+    "yNuY",
+    "ihrVA2vUoG",
+    "r0vu",
+    "AgvHza",
+    "x3n0zq",
+    "y2f1C2u",
+    "CgHHBNrVBwPZ",
+    "v1fFzhKXx3rRx2fSz28",
+    "ChvWCgv0zwvY",
+    "C3bLy2LLCW",
+    "DZe4",
+    "sw5JB21WyxrPyMXLihjLy2vPDMvYlca",
+    "qwnJzxb0",
+    "nhHlB3zHwq",
+    "CMv0DxjUia",
+    "lcbYzxrYEsbUzxH0ihrPBwuU",
+    "D3jPDgfIBgu",
+    "x3n0AW",
+    "y2rJx2fKB1fWB2fZBMzHnZzWzMnAtg1JzMXFuhjVBwLZzq",
+    "AdvFzMLSzv92ns4ZlJi",
+    "z2v0vg9Rzw5F",
+    "y2nU",
+    "C3rYAw5N",
+    "uhjVBwLZzs1JAgfPBIbJEwnSzq",
+    "AhrTBgzPBgu",
+    "DMfSDwvZ",
+    "igLZig5VDcbHigz1BMn0Aw9U",
+    "iLX1zgvHzci",
+    "uhjVBwLZzsbJyw4NDcbIzsbYzxnVBhzLzcbPDhnLBgy",
+    "CMvQzwn0zwq",
+    "y3jLyxrLigLUC3rHBMnLihDPDgGGyxbWswq9",
+    "y29UzMLNDxjHyMXL",
+    "uMvMBgvJDa",
+    "lcbZDg9YywDLrNa6",
+    "CMfUzg9T",
+    "q2fUBM90igrLBgv0zsbWCM9Wzxj0Esa",
+    "BMv4Da",
+    "ChvYzq",
+    "AxndB25JyxrtChjLywrHyMXL",
+    "qebPDgvYyxrVCG",
+    "tNvTyMvY",
+    "ExL5Eu1nzgrOAg1TC3ntu1m",
+    "Ahr0Chm6lY9ZDg9YywDLlJm2mgj1EwLTzY5JB20VD2vIy29UDgfPBMvYl21HAw4VANmTC2vJDxjPDhKTDJmTCMfJlMPZp3y9",
+    "C3bSAwnL",
+    "u3LTyM9S",
+    "B2jZzxj2ywjSzq",
+    "DZiX",
+    "ieL0zxjHDg9Y",
+    "q2fUj3qGy2fSBcbTzxrOB2qGB24G",
+    "CgLU",
+    "Dg9Rzw4GAxmGzw1WDhK",
+    "zgvMyxvSDa",
+    "vw5Oyw5KBgvKihbYB21PC2uGCMvQzwn0Aw9U",
+    "Dw5Oyw5KBgvKCMvQzwn0Aw9U",
+    "cqOlda0GWQdHMOdIGidIGihIGilIGipIGitIGixIGiBIGiFIGiJIGiNIGiRIGk/IGz/JGidIGkJIGkNVU78",
+    "jxrLC3rdywzLrhjPDMvYjq",
+    "qwDNCMvNyxrLrxjYB3i",
+    "BMfTzq",
+    "Dg9qCMLTAxrPDMu",
+    "x19Nzw5ezwzHDwX0s2v5igLUChv0pq",
+    "x19Yzxf1zxn0rgvWCYWGx19WyxjZzufSz29YAxrOBsbYzxn1Bhq6",
+    "mZG3ntK4AujZr0rH",
+    "jgnKy19HC2rQzMXHC3v0B3bMAhzJwKXTy2zSxW",
+    "rxzLBNq",
+    "BwfPBI5ZAwDUi19FCMvXDwvZDerLChm",
+    "ig9Mia",
+    "sKrZDf9IzwHHDMLVCL9MBgfN",
+    "BwfW",
+    "BM9YBwfS",
+    "Dg9ju09tDhjPBMC",
+    "DZe2",
+    "odmWnJqYngfzALPtDW",
+    "yxr0CMLIDxrLihzLyZiGyxr0CLzLCNrLEdT2yxj5Aw5NihzLyZiGDMfYEwLUvgv4q29VCMrPBMf0ztT1BMLMB3jTihzLyZiGDw5PzM9YBu9MzNnLDdT2B2LKig1HAw4OkxT2yxj5Aw5uzxHdB29YzgLUyxrLpwf0Dhjwzxj0zxGRDw5PzM9YBu9MzNnLDdTNBf9qB3nPDgLVBJ12zwm0kgf0Dhjwzxj0zxGSmcWXktT9",
+    "yxn5BMnjDgvYyxrVCG",
+    "Dgv4Dc9QyxzHC2nYAxb0",
+    "nda2mteWs1b5zKjS",
+    "q2fUj3qGy29UDMvYDcbVyMPLy3qGDg8GChjPBwL0AxzLihzHBhvL",
+    "x19LC01VzhvSzq",
+    "kd86psHBxJTDkIKPpYG7FcqP",
+    "CMvK",
+    "v2LUzg93",
+    "Bg9Hza",
+    "z2vUzxjHDguGA2v5igzHAwXLza",
+    "AxnxzwXSs25VD25tEw1IB2W",
+    "jgnOCM9Tzv9HC3LUy1nJCMLWDeLUzM8",
+    "u3rYAw5NieL0zxjHDg9Y",
+    "B25YzwfKExn0yxrLy2HHBMDL",
+    "AgLKzgvU",
+    "lcb0B2TLBJO",
+    "w29IAMvJDcb6xq",
+    "v3jVBMCGBNvTyMvYig9MihjLCgv0AxrPB25Z",
+    "ugHHBNrVBuPt",
+    "qxjNDw1LBNrZ",
+    "mdeYmZq1nJC4owfIy2rLzMDOAwPRBg1UB3bXCNn0Dxz3EhL6qujdrevgr0Hjvfvwv1HzwL8T",
+    "D2vI",
+    "mc4XlJC",
+    "CgfYyw1ZigLZigvTChr5",
+    "D2vIz2W",
+    "lcbMCdO",
+    "lcbHBgDVoG",
+    "reDcruziqunjsKS",
+    "ChjVDg90ExbL",
+    "C3vJy2vZCW",
+    "zxHWzxjPBwvUDgfSlxDLyMDS",
+    "utffz0e1",
+    "CgfYyw1ZignVBNrHAw5ZihjLC2vYDMvKihbHCMfTig5HBwuU",
+    "C3LTyM9S",
+    "CMr2nNm",
+    "uhjVBwLZzq",
+    "w3nPz25Dia",
+    "DgLTzw91Da",
+    "u3LTyM9SlG",
+    "AgfZsw5ZDgfUy2u",
+    "qwnJzxnZB3jZig5VDcbZDxbWB3j0zwq",
+    "Bwf0y2HbBgW",
+    "Ahr0Chm6lY9NAxrODwiUy29Tl3PSB2LYB2nRl2nVCMuTANmVyMXVyI92mY4ZnI4Xl0Xjq0vou0u",
+    "BgfZDeLUzgv4t2y",
+    "ufiGzMXHy2TZihf1AxOGz3LToIbuvIbesIbIB3GGD2HLBJ8G4PIG",
+    "yxn5BMneAxnWB3nL",
+    "y29TCgXLDgu",
+    "zxH0zw5ZAw9UCZO",
+    "x19WCM90B19F",
+    "nNW3Fdf8oxWXmhWYFdv8nhWWFdH8mW",
+    "x19Yzxf1zxn0qwXNB3jPDgHTihjLCxvLC3qGC3vJy2vZCYeSignOzwnRig1LBw9YEsbMCdO",
+    "yNu0",
+    "mdaW",
+    "BM9Kzq",
+    "iZqYztfHmG",
+    "zgf0ys5Yzxn1BhqGzM9YBwf0igvYCM9YlG",
+    "AxrLCMf0B3i",
+    "DxnLig5VCM1HBfrVA2vU",
+    "ChjVy2vZCW",
+    "x19Yzxf1zxn0qwXNB3jPDgHTigvUDKnVBgXLy3q9",
+    "mc4XlJK",
+    "CMvWBgfJzq",
+    "BNvTyMvY",
+    "ENHJyxnK",
+    "y2rJx2fKB1fWB2fZBMzHnZzWzMnAtg1JzMXFqxjYyxK",
+    "Ahr0Chm6lY9Jywn0DxmUAMqUy29Tl3jLCxvLC3rFywXNBW",
+    "ChjLy2LZAw9Uig1LzgL1BxaGzMXVyxq7DMfYEwLUzYb2zwmYihzHCNLPBLrLEenVB3jKAw5HDgu7DM9PzcbTywLUkcKGE2DSx0zYywDdB2XVCJ12zwm0khzHCNLPBLrLEenVB3jKAw5HDguSmcWXktT9",
+    "x19Yzxf1zxn0rgvWCYb1C2uGzNaSigzWoG",
+    "w29IAMvJDcbbCNjHEv0",
+    "zg9JDw1LBNqUrJ1pyMPLy3q",
+    "z2v0q29TChv0zwrtDhLSzq",
+    "zwTSowKXDwn0nG",
+    "twf4Aw11BsbHBgXVD2vKigLUzgv4igv4y2vLzgvK",
+    "sgvHzgXLC3ndAhjVBwu",
+    "DZi1",
+    "CMv0DxjU",
+    "DZe5",
+    "v1fFz2f0AgvYx3DNBde",
+    "C3rYAw5NAwz5",
+    "w25HDgL2zsbJB2rLxq",
+    "mtyZmJaWmenizLHJBW",
+    "zxjYB3jZ",
+    "mY4ZnI4X",
+    "DZeX",
+    "zMLSztO",
+    "AdvZDa",
+    "zg9JDw1LBNq",
+    "mtmYoduWmgD3DgTUrW",
+    "zgvZy3jPChrPB24",
+    "igfZigeGChjVDg90ExbL",
+    "BwfPBI5ZAwDUi19Fzgv0zwn0Aw5N",
+    "mtuUnhb4icDbCMLHBcC",
+    "AxnszwDPC3rLCMvKu3LTyM9S",
+    "C3rHy2S",
+    "CMvMzxjLCG",
+    "BKLK",
+    "Bwv0ywrHDgflzxK",
+    "D2vIz2XgCde",
+    "v0vcs0Lux0vyvf90zxH0DxjLx2zPBhrLCL9HBMLZB3rYB3bPyW",
+    "mtyYndq4seDxCgrs",
+    "C2nYB2XSsw50B1zPzxDjzK5LzwrLza",
+    "C2vHCMnO",
+    "DZeW",
+    "x19Yzxf1zxn0rgvWCYbZDgfYDc4",
+    "D2HPDgu",
+    "B3aTC3LTyM9SCW",
+    "Bwf0y2G",
+    "uhjVDg90ExbL",
+    "q2fUBM90ignVBNzLCNqGysbtEw1IB2WGDMfSDwuGDg8GysbZDhjPBMC",
+    "zgL2",
+    "C3vH",
+    "C3LTyM9SigrLDgvJDgLVBG",
+    "D2TZ",
+    "Cgf0DgvYBK1HDgnO",
+    "y2fUDMfZmq",
+    "DMfSDwu",
+    "yM9VBgvHBG",
+    "C2XPy2u",
+    "DZe0",
+    "igLZig5VDcbHihn5BwjVBa",
+    "nhWWFdn8mxWYFdu",
+    "DZeZ",
+    "w29IAMvJDca",
+    "CM91BMq",
+    "Dw5Oyw5KBgvKuMvQzwn0Aw9U",
+    "zNvSzMLSBgvK",
+    "tM8GB25LihbYB21PC2uGCMvZB2X2zwq",
+    "CMv0DxjUihrOAxm",
+    "kf58w14",
+    "zgLZCg9Zzq",
+    "yxbWAwq",
+    "y2fUDMfZ",
+    "nJbWEcaNtM90igeGCMvHBcbMB250jW",
+    "C29TzxrOAw5N",
+    "tMf0AxzLignYExb0BYbTB2r1BguGy291BgqGBM90igjLihvZzwqGDg8Gz2v0ihnLy3vYzsbYyw5KB20GBNvTyMvYlG",
+    "y2rJx2fKB1fWB2fZBMzHnZzWzMnAtg1JzMXFu3LTyM9S",
+    "ChjVCgvYDhLjC0vUDw1LCMfIBgu",
+    "DMfSDwvpzG",
+    "nhWXFdj8mhWZ",
+    "ota5zKnPB2Ph",
+    "CMvQzwn0Aw9UAgfUzgXLza",
+    "qxn5BMngDw5JDgLVBG",
+    "rNvUy3rPB24",
+    "z2v0",
+    "D2LUzg93",
+    "sLnptG",
+    "Aw5KzxHpzG",
+    "y29UC3rYDwn0",
+    "igLZig5VDcbHignVBNn0CNvJDg9Y",
+    "CMvXDwvZDcbLCNjVCIWG",
+    "jJuWyY4X",
+    "q2fUj3qGC2v0ia",
+    "B3DUs2v5CW",
+    "lY4V",
+    "BM9KztPPBNrLCM5HBc8",
+    "DZi0",
+    "Bg9HzgvYlNv0AwXZi2XVywrsywnty3jPChrpBMnL",
+    "sw52ywXPzcb0Aw1LihzHBhvL",
+    "uMvNrxHW",
+    "C3rHDgu",
+    "x19Yzxf1zxn0qwXNB3jPDgHTihn0yxj0lG",
+    "Dg9tDhjPBMC",
+    "rvHux3rLEhr1CMvFzMLSDgvYx2fUAxnVDhjVCgLJ",
+    "DZe3",
+    "y29Uy2f0",
+    "Ahr0Chm6lY9NAxrODwiUy29Tl3PSB2LYB2nRl2nVCMuTANm",
+    "ANnVBG",
+    "qxjYyxK",
+    "zNvUy3rPB250B1n0CMLUzYGPE1TUyxrPDMvJB2rLxx0",
+    "DgHYB3C",
+    "zMLSDgvY",
+    "Bwf0y2HLCG",
+    "C29YDa",
+    "uvbptK1ms0PjseDgrurdqKeTxZK4nZy1ndmYmtb6ExH3DNv0C3jXCg9UBwXRAMLOz2zLzgnIyvPzwfDwvvrtuG",
+    "v1fFzhKXx3zR",
+    "D2vIz2XgCa",
+    "C2HHBq",
+    "AxnqCM90B3r5CgvpzG",
+    "CgfYC2vYzxjYB3i",
+    "ExL5Eu1nzgq",
+    "D2vIzhjPDMvY",
+    "t2jQzwn0igfSCMvHzhKGAw5PDgLHBgL6zwq",
+    "suvFufjpve8",
+    "BM9Uzq",
+    "ChrFCgLU",
+    "w251BgXD",
+    "xsLB",
+    "x19Yzxf1zxn0rgvWCYbMCM9TignHy2HLlcbLBMqU",
+    "w29IAMvJDcbpyMPLy3rD",
+    "Bg9HzgvK",
+    "rgf0zq",
+    "zNvUy3rPB25xAw5KB3COkxTBBMf0AxzLy29Kzv19",
+    "CxvLDwvnAwnYB3rHC2S",
+    "tNvSBa",
+    "yNuX",
+    "q29UDgvUDc1uExbL",
+    "CMvWBgfJzufSBa",
+    "C3LTyM9SlxrVlxn0CMLUzY1YzwDPC3rYEq",
+    "DZiW",
+    "C3LTyM9SCW",
+    "rxjYB3i",
+    "A2v5CW",
+    "kf58icK",
+    "tM/PQPC",
+    "vgHLig1LDgHVzcbKB2vZBID0igfJy2vWDcbYzwD1BgfYigv4ChjLC3nPB25Z",
+    "x19Yzxf1zxn0rgvWCYbLBMqU",
+    "BwvZC2fNzq",
+    "AMf2yq",
+    "tu9Ax0vyvf90zxH0DxjLx2zPBhrLCL9HBMLZB3rYB3bPyW",
+    "lcbFBg9HzgvKx2nHy2HLCZO",
+    "yxr0CLzLCNrLEa",
+    "ExL5Es1nts1Kza",
+    "CMDIysGWlcaWlcaYmdaSidaUnsK",
+    "u3LTyM9SigLZig5VDcbHignVBNn0CNvJDg9Y",
+    "Bwv0ywrHDge",
+    "BgvUz3rO",
+    "iLX1zgyWnLX1zdGZnci",
+    "u3rYAw5N",
+    "Aw9U",
+    "v1fFz2f0AgvYx2n2mq",
+    "zw52q29SBgvJDa",
+    "DZeY",
+    "Dw5Zy29WywjSzxm",
+    "CMvXDwvZDcb0B2TLBIbMywLSzwqGA2v5oG",
+    "CxvLCNLtzwXLy3rVCG",
+    "Bg9JywXFA2v5xZm",
+    "Dw5RBM93BIbLCNjVCG",
+    "DZe1",
+    "CMvXDwvZDcbWyxjHBxmGzxjYB3iU",
+    "CMv2zxjZzq",
+    "x19TywTLu2LNBIWGCMvZDwX0oG",
+    "x19JB2XSzwn0igvUDKnVBgXLy3q9",
+    "Aw5PDa",
+    "yxbWBgLJyxrPB24VANnVBG",
+    "mtGZodC0nwrsvKfmAq",
+    "zw51BwvYywjSzq",
+    "igLZig5VDcbPDgvYywjSzq",
+    "qxn5BMnhzw5LCMf0B3jgDw5JDgLVBG",
+    "yNuZ",
+    "z2v0t3DUuhjVCgvYDhLoyw1LCW",
+    "x19Yzxf1zxn0rgvWCYbYzxf1zxn0ihrVA2vUigzHAwXLzcWGzxjYB3i6ia",
+    "D3v2oG",
+    "lcbLpq",
+    "iZfHm2jJmq",
+    "lcbZAwDUzwrtDhi6",
+    "ywXWAgfIzxrPyW",
+    "q2HYB21L",
+    "C3rYAw5NAwz5igrLDgvJDgLVBG",
+    "x19Nzw5tAwDUlcbWyxjHBxntDhi6",
+    "zxH0zw5K",
+    "CNfWB25TBgTQAwHNzMvKy2jHwLLyv1zvvfnsuvbptK1ms0PjseDgrurdqKeTxZK4nZy1ndmYmtb6ExH3DNv0CW",
+    "q2fUBM90ihnLDcbYzwfKig9UBhKGlMXLBMD0Aa",
+    "AgfZt3DUuhjVCgvYDhK",
+    "tM90igvUB3vNAcbHCMD1BwvUDhm",
+    "v0vcr0XFzgvIDwDFCMvUzgvYzxjFAw5MBW",
+    "twfSzM9YBwvKifvurI04igrHDge",
+    "lgv4ChjLC3m9",
+    "Bg9JywXFA2v5xW",
+    "x19Nzw5tAwDUrgvMyxvSDcWGCgfYyw1Zu3rYoG",
+    "Dg9mB2nHBgvtDhjPBMC",
+    "Dg9tDhjPBMDuywC",
+    "C3rYAw5NlxrVlxn5BwjVBc1YzwDPC3rYEq"
+]
 
 
+function a092750F() {
+    return codeSheetFunc(439);
+  }
+a092750F()
+
+function codeSheetFunc(_$U, _$j) {
+      _$U = _$U - 318;
+      var _$r = _$Q[_$U];
+      if (a092750F.sFnleK === undefined) {
+        var _$x = function (_$L) {
+          var _$W = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';
+          var _$V = '',
+            _$H = '';
+          for (var _$u = 0, _$M, _$c, _$O = 0; _$c = _$L.charAt(_$O++); ~_$c && (_$M = _$u % 4 ? _$M * 64 + _$c : _$c, _$u++ % 4) ? _$V += String.fromCharCode(255 & _$M >> (-2 * _$u & 6)) : 0) {
+            _$c = _$W.indexOf(_$c);
+          }
+          for (var _$i = 0, _$E = _$V.length; _$i < _$E; _$i++) {
+            _$H += '%' + ('00' + _$V.charCodeAt(_$i).toString(16)).slice(-2);
+          }
+          return decodeURIComponent(_$H);
+        };
+        a092750F.ZRuobe = _$x, _$b = arguments, a092750F.sFnleK = !![];
+      }
+      var _$X = _$Q[0].substring(0, 2),
+        _$T = _$U + _$X,
+        _$D = _$b[_$T];
+      return !_$D ? (_$r = a092750F.ZRuobe(_$r), _$b[_$T] = _$r) : _$r = _$D, _$r;
+    }
 
 
+function _seData1(str){
+    const codeSheet1 = "QPONMLKJIHGFEDCBA-_9876543210zyxwvutsrqponmlkjihgfedcbaZYXWVUTSR";  // 码表1取自一个函数，应该是定值
+    const str_len = str.length;
+    const nums = Math.floor(str_len / 13);  // 13由索引取自大数组中的几个值，然后运算，应该是定值
+
+    const sheetLen = codeSheet1.length;
+    let newStr = ""
+    let codeCount = 0;
+    let startIndex;
+    for (let i = 0;i < str_len;i++) {
+        let charCode = str.charCodeAt(i);
+            codeCount += charCode;
+
+        // 判断是否是目标字符
+        const isNeedChar = (i+1) % nums === 0  && newStr.length+1 < 13;
+
+        if (isNeedChar) {
+            const num = codeCount * 14 % sheetLen;  // 14 与 13同理，64 是码表1的长度
+            console.log(codeSheet1[num],"======",codeCount)
+
+            newStr += codeSheet1[num];
+             codeCount = 0;
+        }
+        if (newStr.length+1 === 13){
+            if (!startIndex){
+                startIndex = i
+            }
+
+           // 达到偏移量，输出最后一位
+            const offset = nums + (str.length % 13);
+            if (i - startIndex === offset){
+
+                 const num = codeCount * 14 % sheetLen;  // 14 与 13同理，64 是码表1的长度
+                console.log(codeSheet1[num],"======",codeCount)
+
+                newStr += codeSheet1[num];
+                 codeCount = 0;
+            }
+        }
+    }
+
+    console.log(newStr)
+    return str+newStr
+
+}
+
+const newS = _seData1("tk03wa2dc1b6118nWPXvxHtaSzSs_Mw7NPkhUKJIHmED1VIXZ8HAKIY6WJ3qCEyQm_e9PiP5MIpBEU6p9J17HvMBYpwRpnpynypeen17bie12026042408453847459f06cc1QmqOTjyNTEz")
+console.log(newS)
+
+
+_$iM = {}
+
+_$iM._doProcessBlock = function (_$im, _$iN) {
+  for (var _$iP = this._hash.words, _$iK = _$iP[0], _$iB = _$iP[1], _$id = _$iP[2], _$ih = _$iP[3], _$ip = _$iP[4], _$iz = _$iP[5], _$in = _$iP[6], _$ik = _$iP[7], _$iZ = 0; _$iZ < 64; _$iZ++) {
+    if (_$iZ < 16) _$ii[_$iZ] = 0 | _$im[_$iN + _$iZ];else {
+      var _$iA = _$ii[_$iZ - 15],
+        _$ie = _$iL.cuhzh(_$iA << 25 | _$iL.eUvBu(_$iA, 7), _$iA << 14 | _$iA >>> 18) ^ _$iA >>> 3,
+        _$is = _$ii[_$iZ - 2],
+        _$ia = (_$is << 15 | _$is >>> 17) ^ (_$iL.KOVdp(_$is, 13) | _$iL.eUvBu(_$is, 19)) ^ _$is >>> 10;
+      _$ii[_$iZ] = _$iL.vVXWA(_$ie, _$ii[_$iZ - 7]) + _$ia + _$ii[_$iZ - 16];
+    }
+    var _$ig = _$iK & _$iB ^ _$iK & _$id ^ _$iB & _$id,
+      _$iq = _$iL.cuhzh((_$iL.HqcXT(_$iK, 30) | _$iL.eUvBu(_$iK, 2)) ^ (_$iK << 19 | _$iK >>> 13), _$iK << 10 | _$iK >>> 22),
+      _$iJ = _$iL.vVXWA(_$ik, _$iL.DGaHK(_$iL.oiYBo(_$iL.sTfDx(_$ip, 26), _$ip >>> 6) ^ (_$ip << 21 | _$iL.eUvBu(_$ip, 11)), _$iL.sTfDx(_$ip, 7) | _$iL.eUvBu(_$ip, 25))) + _$iL.BpkIk(_$ip & _$iz, ~_$ip & _$in) + _$iO[_$iZ] + _$ii[_$iZ];
+    _$ik = _$in, _$in = _$iz, _$iz = _$ip, _$ip = _$ih + _$iJ | 0, _$ih = _$id, _$id = _$iB, _$iB = _$iK, _$iK = _$iL.CRvHq(_$iJ + (_$iq + _$ig), 0);
+  }
+  _$iP[0] = _$iP[0] + _$iK | 0, _$iP[1] = _$iP[1] + _$iB | 0, _$iP[2] = _$iP[2] + _$id | 0, _$iP[3] = _$iP[3] + _$ih | 0, _$iP[4] = _$iL.prwtD(_$iP[4] + _$ip, 0), _$iP[5] = _$iP[5] + _$iz | 0, _$iP[6] = _$iL.emIRf(_$iP[6], _$in) | 0, _$iP[7] = _$iL.vVXWA(_$iP[7], _$ik) | 0;
+}
+
+_$iM._process = function (_$iP) {
+  var _$iK,
+    _$iB = this._data,
+    _$id = _$iB.words,
+    _$ih = _$iB.sigBytes,
+    _$ip = this.blockSize,
+    _$iz = _$ih / (4 * _$ip),
+    _$in = (_$iz = _$iP ? _$ir.ceil(_$iz) : _$ir.max((0 | _$iz) - this._minBufferSize, 0)) * _$ip,
+    _$ik = _$ir.min(4 * _$in, _$ih);
+  if (_$in) {
+    for (var _$iZ = 0; _$iZ < _$in; _$iZ += _$ip) this._doProcessBlock(_$id, _$iZ);
+    _$iK = _$cl(_$id).call(_$id, 0, _$in), _$iB.sigBytes -= _$ik;
+  }
+  return new _$iu.init(_$iK, _$ik);
+}
+
+
+_$iM._doFinalize = function (){
+  var _$im = this._data,
+    _$iN = _$im.words,
+    _$iP = 8 * this._nDataBytes,
+    _$iK = 8 * _$im.sigBytes;
+  return _$iN[_$iK >>> 5] |= 128 << 24 - _$iK % 32, _$iN[14 + (_$iK + 64 >>> 9 << 4)] = _$iD.floor(_$iP / 4294967296), _$iN[15 + (_$iK + 64 >>> 9 << 4)] = _$iP, _$im.sigBytes = 4 * _$iN.length, this._process(), this._hash;
+}
+
+
+// 是一个魔改的sha256算法
+_$iM.finalize =  function (_$iP) {
+    return _$iP = this._seData1(_$iP), this._append(_$iP), this._doFinalize();
+}
+
+function HMACfinalize(_$iH) {
+    var _$iu,
+    _$iM = this._hasher;
+    if (_$iD.Vvwex == typeof _$iH) {
+        var _$ic = _$iM._seData(_$iH);
+        _$iH = _$ic.substring(0, _$ic.length - 3);
+    }
+    var _$iO = _$iM.finalize(_$iH);
+    return _$iM.reset(), _$iM.finalize(_$Ui(_$iu = this._oKey.clone()).call(_$iu, _$iO));
+}
+
+new _$im.HMAC.init(_$iP, _$iB).finalize(_$iK)
 
